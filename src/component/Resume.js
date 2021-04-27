f7be3e7e9f25265c5641cb84b62c6aa9dbc01ff8
Daniel Lucas <danomiterock@yahoo.com>
2021-04-27 10:06:20 -0700

    update styling

--- src/component/Resume.js
+++ src/component/Resume.js
@@ -2,21 +2,37 @@ import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import data from "../contacts.json";
 const styles = {
-  card: { width: "18rem", padding: "20px" },
-  wrapper: { background: "#333333", color: "#CCCCCC" },
+  card: {
+    width: "18rem",
+    padding: "20px",
+    borderRadius: "5%",
+    borderShadow: "5px",
+  },
+  wrapper: { background: "#333333", color: "#CCCCCC", height: "100%", width: "100%" },
 };
 
 const Resume = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col mb={4}>
-          <div style={styles.wrapper}>
-            <a href="assets/resume/Daniel-Lucas-Resume.jpg">Click Here!</a>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div style={styles.wrapper}>
+      <Container fluid>
+        <Row>
+          <Col mb={4}>
+            <Card
+              style={styles.card}
+              key="resume"
+              className="block-example border border-dark"
+            >
+              <Card.Img variant="top" src="assets/image/hiring.jpg" alt="hiring" />
+              <Card.Body>
+                <Card.Title>You're almost there!</Card.Title>
+                <Card.Text>View my resume!</Card.Text>
+                <a href="assets/resume/Daniel-Lucas-Resume.pdf">Click Here!</a>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
