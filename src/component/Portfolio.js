e1656a73fae164941d7e918d89dbf2938c458813
Daniel Lucas <danomiterock@yahoo.com>
2021-04-27 10:06:01 -0700

    update portfolio

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -1,35 +1,50 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import data from "../projects.json"
+import data from "../projects.json";
 const styles = {
-    card: {width: "18rem", padding: "20px"},
-    wrapper: { background: "#333333", color: "#CCCCCC" },
+  card: {
+    width: "18rem",
+    padding: "20px",
+    borderRadius: "5%",
+    borderShadow: "5px",
+  },
+  wrapper: {
+    background: "#333333",
+    color: "#CCCCCC",
+    height: "100%",
+    width: "100%",
+  },
 };
 
-const RenderPortfolio = (props) => {
+const Portfolio = (props) => {
   return (
-    <Container fluid>
-      <Row>
-        <Col mb={4}>
-          <div style={styles.wrapper}>
-            {data.map(portfolio => 
-             (
-            <Card style={styles.card} key={portfolio.id}>
-              <Card.Img variant="top" src={`assets/image/${portfolio.image}`} alt={portfolio.alt}/>
-              <Card.Body>
-                <Card.Title>{portfolio.title}</Card.Title>
-                <Card.Text>
-                  {portfolio.body}
-                </Card.Text>
-                <a href={portfolio.link}>Click Here!</a>
-              </Card.Body>
-            </Card>
-            ))}
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  )};
+    <div style={styles.wrapper}>
+      <Container>
+        <Row>
+          {data.map((portfolio) => (
+            <Col xs={1} md={3} lg={4}>
+              <Card
+                style={styles.card}
+                key={portfolio.id}
+                className="block-example border border-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src={`assets/image/${portfolio.image}`}
+                  alt={portfolio.alt}
+                />
+                <Card.Body>
+                  <Card.Title>{portfolio.title}</Card.Title>
+                  <Card.Text>{portfolio.body}</Card.Text>
+                  <a href={portfolio.link}>Click Here!</a>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-
-export default RenderPortfolio;
+export default Portfolio;
