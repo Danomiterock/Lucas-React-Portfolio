3866029eb6178d214f9eccdd5c22e837eadf4c07
Daniel Lucas <danomiterock@yahoo.com>
2021-04-27 10:41:04 -0700

    center resume card

--- src/component/Resume.js
+++ src/component/Resume.js
@@ -7,7 +7,17 @@ const styles = {
     borderRadius: "5%",
     borderShadow: "5px",
   },
-  wrapper: { background: "#333333", color: "#CCCCCC", height: "100%", width: "100%" },
+  wrapper: {
+    background: "#333333",
+    color: "#CCCCCC",
+    height: "100%",
+    width: "100%",
+  },
+  cardDeck: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  }
 };
 
 const Resume = () => {
@@ -15,13 +25,17 @@ const Resume = () => {
     <div style={styles.wrapper}>
       <Container fluid>
         <Row>
-          <Col mb={4}>
+          <Col mb={4} style={styles.cardDeck}>
             <Card
               style={styles.card}
               key="resume"
               className="block-example border border-dark"
             >
-              <Card.Img variant="top" src="assets/image/hiring.jpg" alt="hiring" />
+              <Card.Img
+                variant="top"
+                src="assets/image/hiring.jpg"
+                alt="hiring"
+              />
               <Card.Body>
                 <Card.Title>You're almost there!</Card.Title>
                 <Card.Text>View my resume!</Card.Text>
