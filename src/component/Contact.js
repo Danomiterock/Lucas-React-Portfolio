72919343b605eb31d7c0901a1f2aaf1dd871787a
Daniel Lucas <danomiterock@yahoo.com>
2021-04-27 10:05:32 -0700

    update contact styles

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,30 +1,43 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
-import data from "../contacts.json"
+import data from "../contacts.json";
 const styles = {
-    card: {width: "18rem", padding: "20px"},
-    wrapper: { background: "#333333", color: "#CCCCCC" },
+  card: {
+    width: "100%",
+    height: "100%",
+    borderRadius: "5%",
+    borderShadow: "5px",
+    textAlign: "center",
+  },
+  wrapper: { background: "#333333", color: "#CCCCCC", height: "100%", width: "100%"  },
 };
 
-const Contact = () => {
+const Contact = (props) => {
   return (
-    <Container fluid>
-      <Row>
-        <Col mb={4}>
-          <div style={styles.wrapper}>
-            {data.map(contacts => 
-             (
-            <Card style={styles.card} key={contacts.id}>
-              <Card.Img variant="top" src={`assets/image/${contacts.image}`} alt={contacts.alt}/>
-              <Card.Body>
-                 <a href={contacts.link}>Click Here!</a>
-              </Card.Body>
-            </Card>
-            ))}
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div style={styles.wrapper}>
+      <Container fluid>
+        <Row>
+          {data.map((contacts) => (
+            <Col xs={1} md={2} lg={3}>
+              <Card
+                style={styles.card}
+                key={contacts.id}
+                className="block-example border border-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src={`assets/image/${contacts.image}`}
+                  alt={contacts.alt}
+                />
+                <Card.Body>
+                  <a href={contacts.link}>Click Here!</a>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
   );
 };
 
